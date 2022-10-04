66e41089e949d7a6417af387d6cc6b79ce943fab
sourabh <sourabhsivare209@gmail.com>
2022-10-04 16:19:36 +0530

    react app with babel and webpack

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Header } from "./Header";
+const App = () => {
+  return (
+    <div>
+        <Header/>
+      <h1>
+        Welcome to React App thats build using Webpack and Babel separately
+      </h1>
+      
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora excepturi obcaecati aliquid necessitatibus nesciunt, esse ea voluptatem asperiores illo reiciendis neque nam at temporibus magnam repellat odio accusantium sapiente iure, cum ducimus maxime? Rerum!
+
+    </div>
+  )
+}
